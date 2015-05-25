817b4cd30ff0c73ec4bf8b94269365f3e81a8617
Pawan <pawansingh9@gmail.com>
2015-05-24 23:47:17 -0500

    Removed irrelevant comments

--- Timely Traveller/src/main/java/com/timelytraveller/destinations/MyResource.java
+++ Timely Traveller/src/main/java/com/timelytraveller/destinations/MyResource.java
@@ -19,9 +19,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONArray;
 
-/**
- * Root resource (exposed at "myresource" path)
- */
 @Path("locations")
 public class MyResource {
 	
